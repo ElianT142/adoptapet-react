0c71d189812b6d17f191c84e12e0d621ab6e447c
ElianT142 <elian.tapia03@inacapmail.cl>
2026-06-25 09:53:30 -0400

    componentes de mascotaCard

---
+++ adoptapet-react/src/components/ListaMascotas.jsx
@@ -0,0 +1,51 @@
+import { mascotas } from './data/mascotas'; // Importa el array
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  // Verifica que el array se carga correctamente
+  console.log('Mascotas:', mascotas);
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <h2>Lista de Mascotas</h2>
+        <ul>
+          {mascotas.map((mascota) => (
+            <li key={mascota.id}>
+              {mascota.nombre} - {mascota.especie}
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  );
+}
+
+export default App;
